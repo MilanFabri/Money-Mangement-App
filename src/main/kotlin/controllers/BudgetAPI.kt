80de5b30bf3125a64509954d432cc5a371cbcef5
MilanFabri <milanfabri05@gmail.com>
2023-11-24 23:46:45 +0000

    Added new model Budget and new controller BudgetAPI

---
+++ src/main/kotlin/controllers/BudgetAPI.kt
@@ -0,0 +1,7 @@
+package controllers
+
+import models.Budget
+
+class BudgetAPI {
+    private var budgets = ArrayList<Budget>()
+}
---
+++ src/main/kotlin/models/Budget.kt
@@ -0,0 +1,6 @@
+package models
+
+data class Budget(var budgetID: Int,
+                  var budgetTitle: String,
+                  var allocatedAmount: Int){
+}
