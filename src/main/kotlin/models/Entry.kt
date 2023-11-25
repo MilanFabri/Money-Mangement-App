da5d717f54abdda9002cbaf792b92dd401da68c9
MilanFabri <milanfabri05@gmail.com>
2023-11-25 20:29:06 +0000

    New model: Entry, no relationship or functionality added yet

---
+++ src/main/kotlin/models/Entry.kt
@@ -0,0 +1,10 @@
+package models
+
+data class Entry(var entryID: Int,
+                 var entryDesc: String,
+                 var location: String,
+                 var dateSpent: Int,
+                 var amountSpent: Int,
+                 var transactionType: String){
+
+}
